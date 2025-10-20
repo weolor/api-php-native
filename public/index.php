6dc5e7e6422f7b7825f4aa22093947b4b8f115dc
Your desir <you@weolor299@gmail.com>
2025-10-20 19:03:19 +0700

    Menambahkan router dan controller modular

---
+++ public/index.php
@@ -0,0 +1,14 @@
+<?php
+use Src\Router;
+use Src\Controllers\UserController;
+
+require __DIR__ . '/../src/Router.php';
+require __DIR__ . '/../src/Controllers/UserController.php';
+
+$router = new Router();
+$userController = new UserController();
+
+$router->add('GET', '/api/v1/users', [$userController, 'index']);
+$router->add('GET', '/api/v1/users/1', fn() => $userController->show(1));
+
+$router->run();
---
+++ src/Router.php
@@ -0,0 +1,25 @@
+<?php
+namespace Src;
+
+class Router {
+    private array $routes = [];
+
+    public function add(string $method, string $path, callable $handler) {
+        $this->routes[] = compact('method', 'path', 'handler');
+    }
+
+    public function run() {
+        $uri = str_replace('/api-php-native/public', '', parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        foreach ($this->routes as $route) {
+            if ($route['method'] === $method && $route['path'] === $uri) {
+                call_user_func($route['handler']);
+                return;
+            }
+        }
+
+        http_response_code(404);
+        echo json_encode(["success" => false, "error" => "Route not found"]);
+    }
+}
